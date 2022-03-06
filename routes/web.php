f736a69fb0269f5626f0a92399fe5b3515e5f6f3
nobuyuki kawada <nobu520113@gmail.com>
2022-03-06 19:25:31 +0900

    コメント追加33

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//aa２２
+//aa２２33
 Route::get('/index', function () {
     return view('index');
 });
