f8fd6b275e96fb197468ed3b73f5f6141f981391
nobuyuki kawada <nobu520113@gmail.com>
2022-07-10 21:44:59 +0900

    後で修正

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -15,7 +15,7 @@ class testController extends BaseController
 	private $service;
 	public function __construct(testService $service)
 	{
-		// コミット(修正)aaaa
+		// コミット(修正)aaaacccccc
 		$this->service = $service;
 	}
 	public function index(Request $request)
